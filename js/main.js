619f00dbc57d1ddaa96ff3e8bda426ce184b0d48
re4nightwing <dulan9595531@gmail.com>
2021-09-11 15:56:37 +0530

    loading screen added

--- js/main.js
+++ js/main.js
@@ -10,3 +10,28 @@ $(document).ready(function() {
       }, 500 );
     });
 });
+
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav .navbar-collapse ul li a");
+const docEnd = document.querySelectorAll("nav .navbar-collapse ul li .contact")[0];
+console.log(sections);
+console.log(navLi);
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 60) {
+      current = section.getAttribute("id"); }
+  });
+
+  navLi.forEach((a) => {
+    console.log(current);
+    a.classList.remove("active");
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      docEnd.classList.add("active");
+    } else if (a.classList.contains(current)) {
+      a.classList.add("active");
+    }
+  });
+};
